227e1c4ad090db33daf20b91d01072e0d9362fc5
sigmoid <1141482126@qq.com>
2015-12-30 00:57:13 +0800

    Update heatS.scala

--- heatS.scala
+++ heatS.scala
@@ -1,2 +1,3 @@
 cnt = pairs.groupByKey.map( t => (t._1, t._2.size)).collect
 links.join(cnt).map( t => (t._1, t._2._1, t._2._2)).collect
+links.join(cnt).join(ranks).map( t => (t._1, t._2._1._1, t._2._1._2, t._2._2)).collect
